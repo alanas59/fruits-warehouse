a648cb0409d79d654f205c7bb3de635032ceb7f1
al-anas <alanas7171@gmail.com>
2022-05-09 01:30:27 +0600

    register component updated

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,67 +1,63 @@
-import React from 'react';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { Link } from 'react-router-dom';
-import auth from '../../../firebase.init';
+import React from "react";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
+import auth from "../../../firebase.init";
 
 const Register = () => {
-    const [
-        createUserWithEmailAndPassword,
-        user,
-        loading,
-        error,
-      ] = useCreateUserWithEmailAndPassword(auth);
-    if(user){
-      console.log(user);
-    }
-    const handleSubmit = event =>{
-        event.preventDefault();
-        const email = event.target.email.value;
-        const password = event.target.password.value;
-        createUserWithEmailAndPassword(email, password,);
-      }
-    return (
-        <div className="container my-4">
-        <div>
-          <h2>Please Register</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <label for="exampleInputEmail1" className="form-label">
-                Email address
-              </label>
-              <input
-                type="email"
-                className="form-control"
-                id="exampleInputEmail1"
-                name="email"
-              />
-            </div>
-            <div className="mb-3">
-              <label for="exampleInputPassword1" className="form-label">
-                Password
-              </label>
-              <input
-                type="password"
-                className="form-control"
-                id="exampleInputPassword1"
-                name="password"
-              />
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Register
-            </button>
-          </form>
-          <div className="mt-2">
-            <p>
-              Already have an account?
-              <Link className="text-danger" to="/login">
-                Please Login
-              </Link>
-            </p>
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+  if (user) {
+    console.log(user);
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    createUserWithEmailAndPassword(email, password);
+  };
+  return (
+    <div className="container my-4">
+      <div>
+        <h2>Please Register</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label for="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              name="email"
+            />
           </div>
-        
+          <div className="mb-3">
+            <label for="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              name="password"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Register
+          </button>
+          <p className="text-danger my-2">{error?.message}</p>
+        </form>
+        <div className="mt-2">
+          <p>
+            Already have an account?
+            <Link className="text-danger" to="/login">
+              Please Login
+            </Link>
+          </p>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Register;
