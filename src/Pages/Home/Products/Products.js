69b6b31bf91685e9b4cbe4ea43a19c4feb38d03a
al-anas <alanas7171@gmail.com>
2022-05-08 15:12:25 +0600

    products component updated

--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Product from "../Product/Product";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
   useEffect(() => {
     fetch("http://localhost:5000/fruits")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+          
+  const handleManageProducts = () =>{
+      navigate('/manage-page');
+  }
   return (
     <div className="container">
       <div className="row g-5 m-5">
@@ -17,6 +23,9 @@ const Products = () => {
           key={product._id}
           product={product}></Product>
         ))}
+      </div>
+      <div className="text-center">
+          <button className="btn btn-info mb-4" onClick={handleManageProducts}>Mange products</button>
       </div>
     </div>
   );
