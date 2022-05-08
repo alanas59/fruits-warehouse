1213a9db528c297f384587c237044815de8f3ffe
al-anas <alanas7171@gmail.com>
2022-05-08 23:29:14 +0600

    require auth component created and implemented

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Footer from './Pages/Shared/Footer/Footer';
-import Header from './Pages/Shared/Header/Header';
-import Home from './Pages/Home/Home/Home';
-import Login from './Pages/Login/Login/Login';
-import Register from './Pages/Login/Register/Register';
-import Inventory from './Pages/Inventory/Inventory';
-import Manage from './Pages/Manage/Manage';
-import AddItem from './Pages/AddItem/AddItem';
-import MyItems from './Pages/MyItems/MyItems';
-import Blogs from './Pages/Blogs/Blogs';
-import NotFound from './Pages/Shared/NotFound/NotFound';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./Pages/Shared/Footer/Footer";
+import Header from "./Pages/Shared/Header/Header";
+import Home from "./Pages/Home/Home/Home";
+import Login from "./Pages/Login/Login/Login";
+import Register from "./Pages/Login/Register/Register";
+import Inventory from "./Pages/Inventory/Inventory";
+import Manage from "./Pages/Manage/Manage";
+import AddItem from "./Pages/AddItem/AddItem";
+import MyItems from "./Pages/MyItems/MyItems";
+import Blogs from "./Pages/Blogs/Blogs";
+import NotFound from "./Pages/Shared/NotFound/NotFound";
+import RequireAuth from "./Pages/Login/RequireAuth/RequireAuth";
 function App() {
   return (
     <div>
-       <Header></Header>
-       <Routes>
-          <Route path='/' element={<Home></Home>}></Route>
-          <Route path='/home' element={<Home></Home>}></Route>
-          <Route path='/login' element={<Login></Login>}></Route>
-          <Route path='/register' element={<Register></Register>}></Route>
-          <Route path='/inventory/:id' element={<Inventory></Inventory>}></Route>
-          <Route path='/manage-page' element={<Manage></Manage>}></Route>
-          <Route path='/add-item' element={<AddItem></AddItem>}></Route>
-          <Route path='/my-items' element={<MyItems></MyItems>}></Route>
-          <Route path='/blogs' element={<Blogs></Blogs>}></Route>
-          <Route path='*' element={<NotFound></NotFound>}></Route>
-       </Routes>
-       <Footer></Footer>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route
+          path="/inventory/:id"
+          element={
+            <RequireAuth>
+              <Inventory></Inventory>
+            </RequireAuth>
+          }
+        ></Route>
+        <Route path="/manage-page" element={<Manage></Manage>}></Route>
+        <Route path="/add-item" element={<AddItem></AddItem>}></Route>
+        <Route path="/my-items" element={<MyItems></MyItems>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Navigate, useLocation } from "react-router-dom";
+import auth from "../../../firebase.init";
+import Loading from "../../Shared/Loading/Loading";
+
+function RequireAuth({ children }) {
+  const [user] = useAuthState(auth);
+  let location = useLocation();
+  if (!user) {
+    return <Loading></Loading>;
+  }
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+}
+export default RequireAuth;
